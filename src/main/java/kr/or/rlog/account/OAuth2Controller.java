2dff5a9e3485bd073b31beef6d8b8a45ddf98d9a
rojae <Jae.Thon96@gmail.com>
2021-03-28 17:21:57 +0900

    FIX) fix

--- src/main/java/kr/or/rlog/account/OAuth2Controller.java
+++ src/main/java/kr/or/rlog/account/OAuth2Controller.java
@@ -1,6 +1,5 @@
 package kr.or.rlog.account;
 
-import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.GetMapping;
 
