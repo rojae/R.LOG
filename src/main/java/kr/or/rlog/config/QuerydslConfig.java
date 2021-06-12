f6a4207047a2077ff11f7910d0c21a7dff521b46
rojae <Jae.Thon96@gmail.com>
2021-06-12 22:55:36 +0900

    ADD) QuerydslConfig.java 추가 (JPAQueryFactory bean 등록)

---
+++ src/main/java/kr/or/rlog/config/QuerydslConfig.java
@@ -0,0 +1,24 @@
+package kr.or.rlog.config;
+
+import com.querydsl.jpa.impl.JPAQueryFactory;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+
+import javax.persistence.EntityManager;
+import javax.persistence.PersistenceContext;
+
+/*
+ * Querydsl을 사용하기 위한 설정
+ * JPAQueryFactory를 Bean으로 등록하여, 재사용
+ */
+@Configuration
+public class QuerydslConfig {
+
+    @PersistenceContext
+    private EntityManager entityManager;
+
+    @Bean
+    public JPAQueryFactory jpaQueryFactory() {
+        return new JPAQueryFactory(entityManager);
+    }
+}
