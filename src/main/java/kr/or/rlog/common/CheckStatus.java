cd568b81e1ffa4270ab06cc8173da597f99eecf6
rojae <Jae.Thon96@gmail.com>
2021-06-20 20:21:29 +0900

    ADD) 공통 상태 클래스 추가 (check, read)

---
+++ src/main/java/kr/or/rlog/common/CheckStatus.java
@@ -0,0 +1,14 @@
+package kr.or.rlog.common;
+
+import lombok.Getter;
+
+/*
+ *
+ * CHECK : 체크
+ * UNCHECK : 미체크
+ */
+@Getter
+public enum CheckStatus {
+    CHECK,
+    UNCHECK;
+}
---
+++ src/main/java/kr/or/rlog/common/ReadStatus.java
@@ -0,0 +1,14 @@
+package kr.or.rlog.common;
+
+import lombok.Getter;
+
+/*
+ *
+ * READ : 읽음
+ * UNREAD : 안읽음
+ */
+@Getter
+public enum ReadStatus {
+    READ,
+    UNREAD;
+}
